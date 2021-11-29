985d860cd5141166b8cbf273764a36d7e328c65f
Cristian Luevanos <cristianlvns@outlook.com>
2021-11-29 01:40:22 -0700

    Backend Express

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-var express = require('express');
-var app = express();
+//Requiere packages and set the port
+const express = require('express');
+const port = 3002;
+//Para permitir manejo de POST y GET
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes');
+const app = express();
 
-app.get('/', function(req, res) {
-	res.send('Hola Mundo!');
-});
+//Usar node.js body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true,
+}));
 
-app.listen(3000, function() {
-	console.log('Aplicación ejemplo, escuchando el puerto 3000!');
+routes(app);
+
+//Iniciar el servidor
+const server = app.listen(port, (error) => {
+	if (error) return console.log(`Error : ${error}`);
+	console.log(`El servidor escucha en el puerto ${server.address().port}`);
 });
---
+++ routes/routes.js
@@ -0,0 +1,37 @@
+//Cargue la conexión del grupo MYSQL
+const { response } = require('express');
+const pool = require('../data/config');
+
+//Ruta de la app
+const router = app => {
+    //Mostrar mensaje de bienvenida de root
+    app.get('/', (request, response) => {
+        response.send({
+            message: 'Bienvenido a Node.js Express REST API!'
+        });
+    });
+    //Mostrar todos los usuarios
+    app.get('/users', (request, response) => {
+        pool.query('SELECT * FROM users', (error, result) => {
+            if (error) throw error;
+            response.send(result);
+        });
+    });
+    //Mostrar un solo usuario por ID
+    app.get('/users/:id', (request, response) => {
+        const id = request.params.id;
+        pool.query('SELECT * FROM users WHERE id = ?', id, (error, result) => {
+            if (error) throw error;
+            response.send(result);
+        });
+    });
+    //Agregar un nuevo usuario
+    app.post('/users', (request, response) => {
+        pool.query('INSERT INTO users SET ?', request.body, (error, result) => {
+            if (error) throw error;
+            response.status(201).send(`User added with ID: ${result.insertId}`);
+        });
+    });
+}
+
+module.exports = router;
